71c3d4a5fa5c3b9882bde73b6d573461202c13e9
Mayya <majko2594@gmail.com>
2023-02-27 11:05:52 +0300

    Task 2_0

---
+++ Seminar_2/2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2. Напишите рпограмму, которая выводит случайное число
+// из отрезка от 1 до 99 и показывает наибольшую цифру числа.
+
+int MaxNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num % 10;
+    if(num1 > num2) return num1;
+    return num2;
+}
+
+int result = MaxNum(new Random().Next(10, 100));
+Console.WriteLine(result);
