4898e7b955e5d91c7fd403ad43144ac6090fb9ec
Mayya <majko2594@gmail.com>
2023-02-27 10:53:24 +0300

    Task 2_1

---
+++ Seminar_2/2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход
+// трехзначное число и на выходе показывает последнюю
+// цифру этого числа
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+
+// int num = new Random().Next(100, 1000); // случайное целое число от 100 до 999
+// Console.WriteLine(num);
+// Console.WriteLine(num % 10); // остаток от деления на 10, выводит последнюю цифру
