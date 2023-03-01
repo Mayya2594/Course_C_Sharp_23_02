03b6e5fc53103e0afd99cc0dcd9ddd5c02a61f52
Mayya <majko2594@gmail.com>
2023-03-01 14:51:45 +0300

    add HomeWork_2 Task_1

---
+++ Seminar_2/HomeWork_2/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int TakeSecondNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 100 / 10;
+}
+
+int result = TakeSecondNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
