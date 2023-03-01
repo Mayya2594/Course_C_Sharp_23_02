2a2ce1884f96427d67699dd5adef6c86c33ada0c
Mayya <majko2594@gmail.com>
2023-03-01 16:26:28 +0300

    add HomeWork_2 Task_4

---
+++ Seminar_2/HomeWork_2/Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void DayOfTheWeek(int num)
+{
+    if(num == 6 || num == 7) Console.WriteLine("Yes");
+    else if(num > 0 && num < 6) Console.WriteLine("No");
+    else Console.WriteLine("Not a day of the week");
+}
+
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+DayOfTheWeek(a);
