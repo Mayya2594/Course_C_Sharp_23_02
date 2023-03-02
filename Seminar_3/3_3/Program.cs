5a5776dd4930a56586319cb22fa0db56737d9b80
Mayya <majko2594@gmail.com>
2023-03-02 11:20:57 +0300

    add Task 3_3

---
+++ Seminar_3/3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3. Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+void Square(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+
+        Console.Write($"{Math.Pow(i, 2)}, ");
+        i ++;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+Square(a);
