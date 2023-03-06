bb895acc18f47f76b6d31dd96fe38e9b75193f6d
Mayya <majko2594@gmail.com>
2023-03-06 10:57:42 +0300

    add Task 4_2

---
+++ Seminar_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+
+int CompositionNum(int num)
+{
+    int comp = 1;
+    for (int i = 1; i <= num; i++)
+        comp = comp * i;
+    return comp;
+}
+
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(N);
+Console.WriteLine(result);
