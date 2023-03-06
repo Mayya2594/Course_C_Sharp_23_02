5aa80280e977ab6b2153d7112ba8e083798e0bff
Mayya <majko2594@gmail.com>
2023-03-06 11:18:13 +0300

    add Task 4_3

---
+++ Seminar_4/4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void Massive (int mas)
+{
+    int[] numbers = new int[mas];
+    for(int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(0, 2);
+        Console.Write($"{numbers[i]}, ");
+    }
+
+}
+
+Massive(8);
