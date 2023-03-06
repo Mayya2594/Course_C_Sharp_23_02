92a0496886b0d402875d65de9aa256921f0873c1
Mayya <majko2594@gmail.com>
2023-03-06 14:34:30 +0300

    add HomeWork_4 Task_4_2

---
+++ Seminar_4/HomeWork_4/Task_4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNum (int num)
+{
+    int sum = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+int A = Math.Abs(int.Parse(Console.ReadLine()!));
+int result = SumNum(A);
+Console.WriteLine($"{A} -> {result}");
