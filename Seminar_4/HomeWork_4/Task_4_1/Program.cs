e8189e1e120802c9d23f0c629550b84ba7cdf2a4
Mayya <majko2594@gmail.com>
2023-03-06 14:03:03 +0300

    add HomeWork_4 Task_4_1

---
+++ Seminar_4/HomeWork_4/Task_4_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Degree (int num1, int num2)
+{
+    int res = 1;
+    for (int i = 1; i <= num2; i++)
+        res = res * num1;
+    return res;
+}
+
+Console.WriteLine("Введите число А:");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B:");
+int B = int.Parse(Console.ReadLine()!);
+int result = Degree(A, B);
+Console.WriteLine($"{A}, {B} -> {result}");
