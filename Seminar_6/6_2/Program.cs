619bff8b0ac70ab6a163ebec0a9c33a3fbab29da
Mayya <majko2594@gmail.com>
2023-03-13 11:00:24 +0300

    add Task 6_2

---
+++ Seminar_6/6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string ConvertBin (int num)
+{
+    string result = String.Empty;
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+string res = ConvertBin(n);
+Console.WriteLine($"{n} -> {res}");
