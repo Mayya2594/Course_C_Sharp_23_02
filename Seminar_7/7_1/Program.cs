aca84d75c296d0954f405a6a64300fc875add716
Mayya <majko2594@gmail.com>
2023-03-16 10:47:29 +0300

    add Task_7_1

---
+++ Seminar_7/7_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1. Задайте двумерный массив размера m на n,
+//    каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+//    Выведите полученный массив на экран.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = i + j;
+    return arr;
+}
+
+
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальную границу:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальную границу:");
+int max = int.Parse(Console.ReadLine()!);
+
+
+int[,] mass = MassNums(rows, columns, min, max);
+Print(mass);
