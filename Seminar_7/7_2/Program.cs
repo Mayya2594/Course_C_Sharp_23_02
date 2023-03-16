90bc82fb0bf1845677672940faf90b7a1655379b
Mayya <majko2594@gmail.com>
2023-03-16 11:02:37 +0300

    add Task_7_2

---
+++ Seminar_7/7_2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, у которых
+// обе позиции чётные, и замените эти элементы на их квадраты.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void FindEven(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i += 2)
+    {    
+        for (int j = 1; j < arr.GetLength(1); j += 2)
+        {    
+            if(i % 2 != 0 && j % 2 != 0)
+                arr[i, j] = arr[i, j] * arr [i, j];
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальную границу:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальную границу:");
+int max = int.Parse(Console.ReadLine()!);
+
+
+int[,] mass = MassNums(rows, columns, min, max);
+Print(mass);
+FindEven(mass);
+Console.WriteLine();
+Print(mass);
