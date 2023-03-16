9def7e0265444358721dd815ffeffcb4f4619957
Mayya <majko2594@gmail.com>
2023-03-16 11:31:52 +0300

    add Task_7_4

---
+++ Seminar_7/7_4/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 4: Задайте двумерный массив. Введите элемент, и найдите
+// первое его вхождение, выведите позиции по горизонтали и
+// вертикали, или напишите, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void FirstNumInput(int[,] arr)
+{
+    Console.WriteLine("Введите число:");
+    int num = int.Parse(Console.ReadLine()!);
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(num == arr[i, j]) 
+            {
+                Console.WriteLine($"Введенный элемент: {num}, результат: [{i + 1}, {j + 1}]");
+                return;
+            }
+        }
+    }    
+    Console.WriteLine ($"Введенный элемент: {num}, результат: такого элемента нет");
+}
+
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальную границу:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальную границу:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(rows, columns, min, max);
+Print(mass);
+FirstNumInput(mass);
