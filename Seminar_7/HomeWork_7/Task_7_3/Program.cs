156f6635b0cc62994c883edc7a3a5e125e59fe83
Mayya <majko2594@gmail.com>
2023-03-18 14:35:39 +0300

    add HomeWork_7 Task_7_3

---
+++ Seminar_7/HomeWork_7/Task_7_3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void AverageColumns (int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for(int j = 0; j < column; j++)
+    {
+        double result = 0;
+        for(int i = 0; i < row; i++)
+            result += arr[i, j];
+        result = Math.Round(result / row, 2);
+        Console.Write($"{result} ");
+    }
+}
+
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальную границу:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальную границу:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(rows, columns, min, max);
+Print(mass);
+AverageColumns(mass);
