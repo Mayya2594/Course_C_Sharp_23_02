75aff54b59aac7a2f365fc35fbd396e6c60e94b8
Mayya <majko2594@gmail.com>
2023-03-22 15:11:46 +0300

    add HomeWork_8 Task_8_1

---
+++ Seminar_8/HomeWork_8/Task_8_1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void SortRow (int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for(int m = 1; m < column; m++)
+        {
+            for(int j = 0; j < column - m; j++)
+            {    
+                if (arr[i, j] < arr[i, j + 1])
+                    (arr[i, j], arr[i, j+1]) = (arr[i, j+1], arr[i,j]);
+            }
+        }
+    }   
+}
+
+
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальную границу:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальную границу:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(rows, columns, min, max);
+Print(mass);
+SortRow(mass);
+System.Console.WriteLine();
+Print(mass);
