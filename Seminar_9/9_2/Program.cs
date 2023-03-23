778ebc79fe3afe4f9e4faf4ecc67088dda63bbe5
Mayya <majko2594@gmail.com>
2023-03-23 11:22:56 +0300

    add Task_9_2

---
+++ Seminar_9/9_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2. Задайте значения M и N. Напишите программу, которая 
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void NumsFromTo(int from, int to)
+{
+    if(to < from) return;
+    NumsFromTo(from, to - 1);
+    Console.Write($"{to}, ");
+}
+
+Console.Write("Введите минимальную границу: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальную границу: ");
+int max = int.Parse(Console.ReadLine()!);
+Console.Write($"{min} to {max} ->  ");
+NumsFromTo(min, max);
