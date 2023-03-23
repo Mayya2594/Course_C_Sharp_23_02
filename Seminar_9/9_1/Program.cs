64d6f1f9759e1cc6cba089d8838931fd5c03dc4b
Mayya <majko2594@gmail.com>
2023-03-23 11:15:19 +0300

    add Task_9_1

---
+++ Seminar_9/9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void SeriesNums(int num)
+{
+    if(num == 0) return;
+    SeriesNums(num - 1);
+    Console.Write($"{num}, ");
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"{N} -> ");
+SeriesNums(N);
