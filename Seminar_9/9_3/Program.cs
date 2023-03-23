c695d0ed1cd17d51b483e4cfda8a34207cecfbb4
Mayya <majko2594@gmail.com>
2023-03-23 11:42:43 +0300

    add Task_9_3

---
+++ Seminar_9/9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+
+int FindSum(int num)
+{
+    if(num == 0) return 0;
+    return num % 10 + FindSum(num / 10);
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(FindSum(N));
